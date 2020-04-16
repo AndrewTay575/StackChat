e7439af3d928226ef86a4f4a1b5d1debdbde5f48
Anthony Guerrero <adguerrero1935@gmail.com>
2020-04-15 20:29:53 -0400

    Fixed title name

--- db/seeds.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO userInfo (firstName, lastName, email)
-VALUES ('Jim', 'Jones', 'JimJones@email.com'), ('Stacy', 'McHaw', 'StacyMcHaw@fakemail.com') , ('Mike', 'Blue', 'MikeBlue@realmail.com')
-INSERT INTO passwords (content, userID)
-VALUES ('password1', (select id from userInfo where id = 1)), ('password2', (select id from userInfo where id = 2))
